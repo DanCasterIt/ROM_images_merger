b0ba6084b346e722c57a4b363c5b3975cd45e54d
Daniele Castro <danielecastro@hotmail.it>
2024-02-06 22:23:19 +0100

    Code refractoring
     * Code refractoring
     * Lint code using `clang-format`
     * Slightly change printf()s messages
     * Update Windows executable
     * Update README file

--- README.md
+++ README.md
@@ -1,43 +1,45 @@
 # What does this program
-This program creates a file made of n partitions equally sized and puts in these partitions the files specified in the arguments. Blank spaces will be filled with 0xFF or 0x00 values.
+This program creates a file made of n partitions equally sized and puts in these partitions the files specified in the arguments. Blank spaces will be filled with `0xFF` or `0x00` values.
 The user can choose:
 * which file goes in each partition
 * the lenght of the final image
 * the number of partitions
 
 # Why did I make this program and what is it thought for
-I made this simple program because I erroneously bought some W27C512-45Z EEPROMs for my Acorn BBC Master Compact, an old 8-bit PC.
-It has 5 EPROM slots. When buying my W27C512-45Z EEPROMs I didn't know that only the system OS ROM slot is 64kbyte capable and that all the others are only 16kbyte capable exception made for one that is 32kbyte capable.
-This means that I should have had buy some D27128 EPROMs instead of W27C512-45Z EEPROMs.
+I made this simple program because I erroneously bought some `W27C512-45Z` EEPROMs for my Acorn BBC Master Compact, an old 8-bit PC.
+It has 5 EPROM slots. When buying my `W27C512-45Z` EEPROMs I didn't know that only the system OS ROM slot is 64kbyte capable and that all the others are only 16kbyte capable exception made for one that is 32kbyte capable.
+This means that I should have had buy some `D27128` EPROMs instead of `W27C512-45Z` EEPROMs.
 
-Anyway, since W27C512-45Z are four time bigger than D27128 I decided to unplug the two A13 and A14 address lines from the ROM socket and connect a switch to them so that I can switch between 4 ROM images using the same ROM IC. In the Acorn BBC Master Compact PCB A13 and A14 are tied to Vcc. The two switches per ROM IC should connect the two address PINs to GND or Vcc depending on which image in the ROM I would like to access.
+Anyway, since `W27C512-45Z` are four time bigger than `D27128` I decided to unplug the two `A13` and `A14` address lines from the ROM socket and connect a switch to them so that I can switch between 4 ROM images using the same ROM IC. In the Acorn BBC Master Compact PCB `A13` and `A14` are tied to `Vcc`. The two switches per ROM IC should connect the two address `PIN`s to `GND` or `Vcc` depending on which image in the ROM I would like to access.
 
 This tool is thought to merge four 16kbyte ROM images into a sigle one for a 64kbyte ROM but these parameters can be modified.
 
 # How to compile
-`gcc ROM_image_generator.c -o ROM_image_generator`
+```bash
+gcc ROM_image_generator.c -o ROM_image_generator
+```
 
 It works both in Linux and Windows. A 32-bit Windows binary is provided in this repo.
 
 # How to use it, example
 ```
-> ROM_image_generator DFSimage EDITROM basiced
-Please, specify the memory size in bytes. (0 for 65536): 0
-Please, specify the number of equally sized partitions. (0 for 4): 0
-Please, specify if to fill blank partitions with ones (1) or zeros (0): 1
+$ ../ROM_image_generator utils basic48 adfs210
+Input the memory size in bytes. (0 for 65536): 0
+Input the number of equally sized partitions. (0 for 4): 0
+Input if to fill blank partitions with ones (1) or zeros (0): 1
 Found 3 files. Choose where to place them.
 partition 3 @ 0x00000000-0x00003FFF filled with FFs
-partition 2 @ 0x00004000-0x00007FFF filled with file: basiced
-partition 1 @ 0x00008000-0x0000BFFF filled with file: EDITROM
-partition 0 @ 0x0000C000-0x0000FFFF filled with file: DFSimage
+partition 2 @ 0x00004000-0x00007FFF filled with file: adfs210
+partition 1 @ 0x00008000-0x0000BFFF filled with file: basic48
+partition 0 @ 0x0000C000-0x0000FFFF filled with file: utils
 Apply changes? (y or n): n
-Please, select which partition to swap.
-Partition number: 1
-goes to number: 3
-partition 3 @ 0x00000000-0x00003FFF filled with file: EDITROM
-partition 2 @ 0x00004000-0x00007FFF filled with file: basiced
+Input which partition to swap.
+Partition number: 3
+goes to number: 1
+partition 3 @ 0x00000000-0x00003FFF filled with file: basic48
+partition 2 @ 0x00004000-0x00007FFF filled with file: adfs210
 partition 1 @ 0x00008000-0x0000BFFF filled with FFs
-partition 0 @ 0x0000C000-0x0000FFFF filled with file: DFSimage
+partition 0 @ 0x0000C000-0x0000FFFF filled with file: utils
 Apply changes? (y or n): y
 Writing "ROM_image.bin"...
 ```
--- ROM_image_generator.c
+++ ROM_image_generator.c
@@ -1,85 +1,104 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 
-typedef struct partitions_s	{
-	uint16_t start;
-	uint16_t end;
-	char *name;
+typedef struct partitions_s {
+  uint16_t start;
+  uint16_t end;
+  char *name;
 } partitions_t;
 
-int main(int argc, char *argv[])	{
-	FILE *fdin, *fdout;
-	char *tmp, ch = 'n';
-	int INC, PARTITIONS_NUMBER, ROM_SIZE, i, a, b;
-	uint8_t filler;
-	printf("Please, specify the memory size in bytes. (0 for 65536): ");
-	scanf("%d", &ROM_SIZE);
-	getchar();
-	if(ROM_SIZE <= 0) ROM_SIZE = 65536;
-	printf("Please, specify the number of equally sized partitions. (0 for 4): ");
-	scanf("%d", &PARTITIONS_NUMBER);
-	getchar();
-	if(PARTITIONS_NUMBER <= 0) PARTITIONS_NUMBER = 4;
-	do	{
-		printf("Please, specify if to fill blank partitions with ones (1) or zeros (0): ");
-		scanf("%d", &filler);
-		getchar();
-		if(filler != 1 && filler != 0)	printf("Please, input 1 or 0.\n");
-	} while(filler != 1 && filler != 0);
-	if(filler == 1) filler = 0xFF;
-	else filler = 0x00;
-	if(!(argc <= PARTITIONS_NUMBER + 1)) return -1;
-	INC = ROM_SIZE/PARTITIONS_NUMBER;
-	uint8_t buffer[INC];
-	partitions_t part[PARTITIONS_NUMBER];
-	for(i = 0; i < PARTITIONS_NUMBER; i++)	{
-		part[PARTITIONS_NUMBER - 1 - i].start = i * INC;
-		part[PARTITIONS_NUMBER - 1 - i].end = ((i + 1) * INC) - 1;
-		part[PARTITIONS_NUMBER - 1 - i].name = NULL;
-	}
-	for(i = 1; i < argc; i++)	part[i - 1].name = argv[i];
-	printf("Found %d files. Choose where to place them.\n", argc - 1);
-	while(ch != 'y')	{
-		for(i = PARTITIONS_NUMBER - 1; i >= 0; i--)	{
-			printf("partition %d ", i);
-			if(part[i].name != NULL)	printf("@ 0x%08X-0x%08X filled with file: %s\n", part[i].start, part[i].end, part[i].name);
-			else	printf("@ 0x%08X-0x%08X filled with %02Xs\n", part[i].start, part[i].end, filler);
-		}
-		printf("Apply changes? (y or n): ");
-		scanf("%c", &ch);
-		getchar();
-		if(ch == 'y') continue;
-		printf("Please, select which partition to swap.\n");
-		do	{
-			printf("Partition number: ");
-			scanf("%d", &a);
-			getchar();
-			if(a >= PARTITIONS_NUMBER) printf("Please, input a number less than %d.\n", PARTITIONS_NUMBER);
-		} while(a >= PARTITIONS_NUMBER);
-		do	{
-			printf("goes to number: ", i);
-			scanf("%d", &b);
-			getchar();
-			if(b >= PARTITIONS_NUMBER) printf("Please, input a number less than %d.\n", PARTITIONS_NUMBER);
-		} while(b >= PARTITIONS_NUMBER);
-		tmp = part[a].name;
-		part[a].name = part[b].name;
-		part[b].name = tmp;
-	}
-	printf("Writing \"ROM_image.bin\"...\n");
-	fdout = fopen("ROM_image.bin", "wb");
-	if(fdout == NULL) return -1;
-	for(i = PARTITIONS_NUMBER - 1; i >= 0; i--)	{
-		memset((void*)buffer, filler, INC);
-		if(part[i].name != NULL)	{
-			fdin = fopen(part[i].name, "rb");
-			if(fdin == NULL) return -1;
-			if(fread((void*)buffer, 1, INC, fdin) <= 0) return -1;
-			if(fwrite((void*)buffer, 1, INC, fdout) <= 0) return -1;
-			fclose(fdin);
-		} else	if(fwrite((void*)buffer, 1, INC, fdout) <= 0) return -1;
-	}
-	fclose(fdout);
-	return 0;
+int main(int argc, char *argv[]) {
+  FILE *fdin, *fdout;
+  char *tmp, ch = 'n';
+  int inc, partitions_number, rom_size, i, a, b;
+  uint8_t filler;
+  printf("Input the memory size in bytes. (0 for 65536): ");
+  scanf("%d", &rom_size);
+  getchar();
+  if (rom_size <= 0)
+    rom_size = 65536;
+  printf("Input the number of equally sized partitions. (0 for 4): ");
+  scanf("%d", &partitions_number);
+  getchar();
+  if (partitions_number <= 0)
+    partitions_number = 4;
+  do {
+    printf("Input if to fill blank partitions with ones (1) or zeros (0): ");
+    scanf("%hhu", &filler);
+    getchar();
+    if (filler != 1 && filler != 0)
+      printf("Input 1 or 0.\n");
+  } while (filler != 1 && filler != 0);
+  if (filler == 1)
+    filler = 0xFF;
+  else
+    filler = 0x00;
+  if (argc > partitions_number + 1)
+    return -1;
+  inc = rom_size / partitions_number;
+  uint8_t buffer[inc];
+  partitions_t part[partitions_number];
+  for (i = 0; i < partitions_number; i++) {
+    part[partitions_number - 1 - i].start = i * inc;
+    part[partitions_number - 1 - i].end = ((i + 1) * inc) - 1;
+    part[partitions_number - 1 - i].name = NULL;
+  }
+  for (i = 1; i < argc; i++)
+    part[i - 1].name = argv[i];
+  printf("Found %d files. Choose where to place them.\n", argc - 1);
+  while (ch != 'y') {
+    for (i = partitions_number - 1; i >= 0; i--) {
+      printf("partition %d ", i);
+      if (part[i].name != NULL)
+        printf("@ 0x%08X-0x%08X filled with file: %s\n", part[i].start,
+               part[i].end, part[i].name);
+      else
+        printf("@ 0x%08X-0x%08X filled with %02Xs\n", part[i].start,
+               part[i].end, filler);
+    }
+    printf("Apply changes? (y or n): ");
+    scanf("%c", &ch);
+    getchar();
+    if (ch == 'y')
+      continue;
+    printf("Input which partition to swap.\n");
+    do {
+      printf("Partition number: ");
+      scanf("%d", &a);
+      getchar();
+      if (a >= partitions_number)
+        printf("Input a number less than %d.\n", partitions_number);
+    } while (a >= partitions_number);
+    do {
+      printf("goes to number: ");
+      scanf("%d", &b);
+      getchar();
+      if (b >= partitions_number)
+        printf("Input a number less than %d.\n", partitions_number);
+    } while (b >= partitions_number);
+    tmp = part[a].name;
+    part[a].name = part[b].name;
+    part[b].name = tmp;
+  }
+  printf("Writing \"ROM_image.bin\"...\n");
+  fdout = fopen("ROM_image.bin", "wb");
+  if (fdout == NULL)
+    return -1;
+  for (i = partitions_number - 1; i >= 0; i--) {
+    memset((void *)buffer, filler, inc);
+    if (part[i].name != NULL) {
+      fdin = fopen(part[i].name, "rb");
+      if (fdin == NULL)
+        return -1;
+      if (fread((void *)buffer, 1, inc, fdin) <= 0)
+        return -1;
+      if (fwrite((void *)buffer, 1, inc, fdout) <= 0)
+        return -1;
+      fclose(fdin);
+    } else if (fwrite((void *)buffer, 1, inc, fdout) <= 0)
+      return -1;
+  }
+  fclose(fdout);
+  return 0;
 }
